5e6d47b1bcf3cca6366cb9bf651e2c5589b1b060
Olena Avdiunina <elena.avdiunina@gmail.com>
2022-10-06 13:50:05 +0200

    fixed

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -12,7 +12,8 @@ const Index = ({ orders, products }) => {
     console.log(id);
     try {
       const res = await axios.delete(
-        "http://localhost:3000/api/products/" + id
+        "https://pizza-order-hot-pizza.vercel.app/api/products/api/products/" +
+          id
       );
       setPizzaList(pizzaList.filter((pizza) => pizza._id !== id));
     } catch (err) {
@@ -25,9 +26,12 @@ const Index = ({ orders, products }) => {
     const currentStatus = item.status;
 
     try {
-      const res = await axios.put("http://localhost:3000/api/orders/" + id, {
-        status: currentStatus + 1,
-      });
+      const res = await axios.put(
+        "https://pizza-order-hot-pizza.vercel.app/api/orders/" + id,
+        {
+          status: currentStatus + 1,
+        }
+      );
       setOrderList([
         res.data,
         ...orderList.filter((order) => order._id !== id),
@@ -129,8 +133,12 @@ export const getServerSideProps = async (ctx) => {
     };
   }
 
-  const productRes = await axios.get("http://localhost:3000/api/products");
-  const orderRes = await axios.get("http://localhost:3000/api/orders");
+  const productRes = await axios.get(
+    "https://pizza-order-hot-pizza.vercel.app/api/products"
+  );
+  const orderRes = await axios.get(
+    "https://pizza-order-hot-pizza.vercel.app/api/orders"
+  );
 
   return {
     props: {
